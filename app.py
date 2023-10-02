72abacb668d14e37a3482796410068345d502c1c
sabaamanat <117507135+sabaamanat@users.noreply.github.com>
2023-10-02 13:47:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'default_user')
 app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', 'default_password')
 app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'default_db')
 
-# Initialize MySql
+# Initialize MySQL
 mysql = MySQL(app)
 
 @app.route('/')
